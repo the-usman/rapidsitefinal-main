86e28692e2802b9f9b297932bd2f7dfe6da11827
Usman Javed <2022CS620@student.uet.edu.pk>
2024-08-24 16:49:42 +0500

    Update AnimatedMenuItems.tsx

--- components/AnimatedMenuItems.tsx
+++ components/AnimatedMenuItems.tsx
@@ -1,29 +1,14 @@
 import React from 'react';
-import { CSSTransition } from 'react-transition-group';
-import './MenuItems.css'; // Ensure this path matches your project structure
 
-const MenuItems: React.FC = () => {
+
+
+
+
+const AnimatedMenuItems = () => {
     return (
-        <div className="pb-4 pt-0">
-            {/* Your MenuItems code */}
+        <div>
+            hello
         </div>
-    );
-};
-
-interface MenuProps {
-    isResourcesOpen: boolean;
-}
-
-const AnimatedMenuItems: React.FC<MenuProps> = ({ isResourcesOpen }) => {
-    return (
-        <CSSTransition
-            in={isResourcesOpen}
-            timeout={300}
-            classNames="menu-items"
-            unmountOnExit
-        >
-            <MenuItems />
-        </CSSTransition>
     );
 };
 
